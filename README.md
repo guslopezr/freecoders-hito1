0cf0eb2db45af9027eed398e4c26f5f3b27e78ff
guslopezr <guslopezr@gmail.com>
2023-03-04 10:01:55 -0300

    cambios en readme y orden del menú

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ El proyecto contiene funcionalidades de Frontend necesarias para recorrer las vi
 Los estilos están en desarrollo y mejora, aunque adecuados para revisión.
 La responsividad no ha sido depurada completamente. Esto se completará en el Deploy.
 
+### Sobre la fuente "Consolas"
+El frontend considera el uso de la fuente Consolas, la cual eventualmente podría no estar en la máquina donde se visualiza.
+De ser el caso, recomendamos instalarla descargándola desde este link: https://www.fontpalace.com/font-details/Consolas/
+
+[Form](form.png)
+
 
 ### Acceso a repositorio
 https://proyecto-freecoders-hito1.netlify.app/
--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,24 +1,47 @@
-import {NavLink } from 'react-router-dom'
-import './styles/navegacion.css'
+import { NavLink } from "react-router-dom";
+import "./styles/navegacion.css";
 
-const changing = ({ isActive }) => isActive ? 'isActive' : 'mainNavBar'
+const changing = ({ isActive }) => (isActive ? "isActive" : "mainNavBar");
 
 const Navegacion = () => {
-    return (
-        <div className='mainNavBarContainer'>
-            <NavLink to='/perfilfreecoder' className={changing}>Perfil</NavLink>
-            <NavLink to='/busqueda' className={changing}>Búsqueda</NavLink>
-            <NavLink to='/contactarfreecoder' className={changing}>Contactar</NavLink>
-            <NavLink to='/confirmarorden' className={changing}>Confirmar Orden</NavLink>
-            <NavLink to='/seguimiento' className={changing}>Seguimiento</NavLink>
-            <NavLink to='/home' className={changing}>Home</NavLink>
-            <NavLink to='/crearperfil' className={changing}>Crear Perfil</NavLink>
-            <NavLink to='/login' className={changing}>Login</NavLink>
-            <NavLink to='/enviopropuesta' className={changing}>Envio propuesta</NavLink>
-            <NavLink to='/crearpropuesta' className={changing}>Crear propuesta</NavLink>
-            <NavLink to='/abonarpropuesta' className={changing}>Abonar Propuesta</NavLink>
-        </div>
-    )
-}
+  return (
+    <div className="mainNavBarContainer">
+      <NavLink to="/crearperfil" className={changing}>
+        Crear Perfil
+      </NavLink>
+      <NavLink to="/perfilfreecoder" className={changing}>
+        Perfil
+      </NavLink>
+      <NavLink to="/busqueda" className={changing}>
+        Búsqueda
+      </NavLink>
+      <NavLink to="/contactarfreecoder" className={changing}>
+        Contactar
+      </NavLink>
+      <NavLink to="/confirmarorden" className={changing}>
+        Orden en proceso
+      </NavLink>
+    
+      <NavLink to="/seguimiento" className={changing}>
+        Seguimiento
+      </NavLink>
+      <NavLink to="/crearpropuesta" className={changing}>
+        Crear propuesta
+      </NavLink>
+      <NavLink to="/enviopropuesta" className={changing}>
+        Envio propuesta
+      </NavLink>
+      <NavLink to="/abonarpropuesta" className={changing}>
+        Abonar Propuesta
+      </NavLink>
+      <NavLink to="/home" className={changing}>
+        Home
+      </NavLink>
+      <NavLink to="/login" className={changing}>
+        Login
+      </NavLink>
+    </div>
+  );
+};  
 
-export default Navegacion
+export default Navegacion;
